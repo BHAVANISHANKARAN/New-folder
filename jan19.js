a0b11d4346d3e93af2c4df433953b5b6eb6ad949
BHAVANISHANKARAN <bhavanishankaran2003@gmail.com>
2024-01-19 21:29:37 +0530

    for loops , array methods

--- jan19.js
+++ jan19.js
@@ -4,3 +4,59 @@
 
 //Annonymous, Arrow, IIFE
 ((n) => n * 2)(10);
+
+//exercise
+
+//ex1
+
+const person = {
+  name: {
+    first: "Alice",
+    last: "Johnson",
+  },
+  age: 28,
+  favoriteColors: ["blue", "green"],
+};
+
+const getPersonInfo1 = (p) => {
+  const {
+    name: { first: firstName, last: lastName },
+    age: personAge,
+    favoriteColors: [primaryColor, secondaryColor],
+  } = p;
+
+  return `${firstName} ${lastName} is ${personAge} years old. Her favorite colors are ${primaryColor} and ${secondaryColor}.`;
+};
+
+console.log(getPersonInfo1(person));
+//
+
+const cart = [
+  { name: "Apple", price: 0.5, quantity: 4 },
+  { name: "Banana", price: 0.25, quantity: 6 },
+];
+
+const newItems = [
+  { name: "Cherry", price: 0.75, quantity: 5 },
+  { name: "Date", price: 1, quantity: 3 },
+];
+
+let calculateTotal = (x) => {
+  let total = 0;
+  for (var { price, quantity } of x) {
+    total += price * quantity;
+  }
+  return total;
+};
+console.log(calculateTotal([...newItems, ...cart]));
+
+//
+
+// Ex 4:
+//   40 and above is Pass | 'All Pass' | 'Fail'
+const marks = [80, 68, 90, 40, 30, 50];
+if (marks.every((p) => p >= 40)) {
+  console.log("All pass");
+} else {
+  console.log("all fail");
+}
--- notesJan19.md
+++ notesJan19.md
@@ -35,3 +35,55 @@ Object.keys(salary) // ["john","pete","mary"] - array of strings
 Object.values(salary) //[500,400,250] - type number
 
 ```
+
+# shortcuts
+
+F2 - to change variable name in all places
+
+# array methods
+
+- Will it mutate the array ?
+- ## what datatype will be returned ?
+
+- Array.includes() - returns boolean
+- Array.join() - comma is the default seperator
+- Array.split() - splits the values based on a symbol
+- Arrays.slice -
+- Arrays.every(e => === " ") - built in higher order function
+- Arrays.some(p => p === " ") -
+
+- Arrays.unshift() - adds elements in the front of the array
+- Arrays.push() - adds elements in the rear of the array
+
+- chrome developer tools
+- immutable js
+
+- reassignment is not possible in const
+
+---
+
+# optional chaining :
+
+```js
+const person1 = {
+  name: {
+    first: "ajay",
+    last: "kumar",
+  },
+  age: 28,
+  favouriteColors: ["blue", "green"],
+  // place:{
+  //     city:"chennai",
+  // },
+};
+
+//console.log(person1.place.city) //cannot read properties of undefined
+
+// Defence code
+if (person1.place && person1.place.city) {
+  console.log(person1.place.city);
+}
+
+//?. optional chaining
+console.log(person1?.place?.city);
+```
